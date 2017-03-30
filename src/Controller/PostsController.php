e0849e1db327b063708878cc0b6cd100221ee6f5
root <root@localhost.localdomain>
2017-03-30 22:40:59 +0000

    recent tickets view added

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -7,7 +7,10 @@ use Cake\ORM\TableRegistry;
 class PostsController extends AppController {
   public function index() {
     $this->Post = TableRegistry::get('Posts');
+    $this->Ticket = TableRegistry::get('Tickets');
     $posts = $this->Post->find()->all();
+    $tickets = $this->Ticket->find()->all();
     $this->set('posts', $posts);
+    $this->set('tickets', $tickets);
   }
 }
