7b06e57b4e9e1b8c6600a414e58e10695d6e9a64
root <root@localhost.localdomain>
2017-04-24 06:02:31 +0000

    added to PostsController concern unions searched result about open tickets

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -36,9 +36,24 @@ class PostsController extends AppController {
 
     $posts = $this->Post->find()->all();
     $tickets = $this->Ticket->find()
-			->order(['last_update' => 'DESC'])
-			->limit(5);
- 
+		->order(['last_update' => 'DESC'])
+		->limit(5);
+/*
+    //openチケットが3以上含まれているかのチェック(CheckTicketControllerをモックに置き換える)
+    $check_ticket = new CheckTicketController();
+    $open_ticket_num = checkTicket($tickets); 
+
+    if($open_ticket_num < 3){
+    	$tickets = $this->Ticket->find()
+		->order(['last_update' => 'DESC'])
+		->limit(3);
+    	//上記クエリで取得されたチケット以外を指定するサブクエリを作る
+        $tickets_union = $this->Ticket->find()
+		->order(['last_update' => 'DESC'])
+		->limit(2);
+
+    }
+ */
     $users = $this->paginate($this->Post);
     $tasks = $this->Post->find()
 		->select(['work']);
