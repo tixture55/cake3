dbb482ceea12d8723cd984b0e4614ab4595bd591
root <root@localhost.localdomain>
2017-05-01 11:17:10 +0000

    added __construct() to  InsertController

--- src/Controller/InsertPostController.php
+++ src/Controller/InsertPostController.php
@@ -5,54 +5,65 @@ use Cake\ORM\TableRegistry;
 final class InsertPostController extends InsertController{
 
 
+	protected $reply;
+	protected $post;
+
+        function __construct(){
+		parent::__construct();
+
+		$reply = $this->Ticket_replies->newEntity();
+		$this->reply = $reply;
+
+		$post = $this->Ticket->newEntity();
+		$this->post = $post;
+	}	
+
 	//画面によって、セットするタイトルを変える
 	public function postTicketReply($req , $ticket_id){
 
 
-		$reply = $this->Ticket_replies->newEntity();
-		$reply->details = $req->data('detail'); 
+		$this->reply->details = $req->data('detail'); 
 
 		if($req->data('status') == 0){
-			$reply->status = "open";
+			$this->reply->status = "open";
 		}else{
-			$reply->status ="close";
+			$this->reply->status ="close";
 		}
 
-		$reply->target_name = $req->data('target_name');
-		$reply->posts_id = $ticket_id;
-		$reply->last_update = date('Y/m/d H:i:s');
-		$this->Ticket_replies->save($reply);
+		$this->reply->target_name = $req->data('target_name');
+		$this->reply->posts_id = $ticket_id;
+		$this->reply->last_update = date('Y/m/d H:i:s');
+		$this->Ticket_replies->save($this->reply);
 	}
 	
         public function postTicket($req){
 
-		$post = $this->Ticket->newEntity();
-		$post->task_id = $req->data('works'); 
-		$post->posts_id = $req->data('works'); 
-		$post->details = $req->data('detail'); 
+		$this->post->task_id = $req->data('works'); 
+		$this->post->posts_id = $req->data('works'); 
+		$this->post->details = $req->data('detail'); 
 
     		$tickets = $this->Ticket->find()
 			->select(['task_id','details'])
-			->where(['Tickets.details' => $post->details]);
-		$post->title  = $req->data('ticket_title'); 
+			->where(['Tickets.details' => $this->post->details]);
+		$this->post->title  = $req->data('ticket_title'); 
 		
 		if($req->data('status') == 0){
-			$post->status = "open";
+			$this->post->status = "open";
 		}else{
-			$post->status ="close";
+			$this->post->status ="close";
 		}
 
-		$post->target_name = $req->data('target_name');
-		$post->last_update = date('Y/m/d H:i:s');
-		$post->deadline = date('Y/m/d H:i:s');
+		$this->post->target_name = $req->data('target_name');
+		$this->post->last_update = date('Y/m/d H:i:s');
+		$this->post->deadline = date('Y/m/d H:i:s');
 		
 		if($tickets->count() > 0){
 		
 		}else{
-		 	$this->Ticket->save($post);
+		 	$this->Ticket->save($this->post);
 		}
 				
-		return $post;
+		return $this->post;
 	}
 
 
